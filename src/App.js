3d78fe6d6bf2b6015ab8eb6aa94cee81378c7d33
Yuji Kan <gtenio@gmail.com>
2020-03-20 01:35:32 +0900

    del Input.js modified Form.js App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
 import Title from "./components/Title";
 import Form from "./components/Form";
-import Input from "./components/Input";
 import "./App.css";
 
 // const CALORIE_LOWER_LIMIT = 1.3;
@@ -21,84 +20,13 @@ import "./App.css";
 // };
 
 function App() {
-  const [values, setValues] = useState({
-    basalMetabolicRate: 0,
-    intakeCoefficient: 0,
-    bodyWeight: 0,
-    bodyFat: 0,
-    proteinCoefficient: 0,
-    mealTimes: 0
-  });
-
-  function handleInputChange(event) {
-    const target = event.target;
-    const value = target.type === "checkbox" ? target.checked : target.value;
-    const name = target.name;
-    console.log(value);
-    setValues({ ...values, [name]: value });
-  }
-
   return (
     <>
       <Title title="PFCバランス計算" />
       <div className="container">
-        <Form>
-          <Input
-            type="number"
-            name="basalMetabolicRate"
-            title="基礎代謝(1000-4000kcal)"
-            required={true}
-            minValue={1000}
-            maxValue={4000}
-            onChange={handleInputChange}
-          />
-          <Input
-            type="number"
-            name="intakeCoefficient"
-            title="摂取係数(1.3-1.7)"
-            min={1.3}
-            maxValue={1.7}
-            required={true}
-            onChange={handleInputChange}
-          />
-          <Input
-            type="number"
-            name="bodyWeight"
-            title="体重(kg)"
-            minValue={40}
-            maxValue={90}
-            required={true}
-            onChange={handleInputChange}
-          />
-          <Input
-            type="number"
-            name="bodyFat"
-            title="体脂肪(%)"
-            minValue={3}
-            maxValue={25}
-            required={true}
-            onChange={handleInputChange}
-          />
-          <Input
-            type="number"
-            name="proteinCoefficient"
-            title="タンパク質摂取係数(2-2.5)"
-            minValue={2}
-            maxValue={2.5}
-            required={true}
-            onChange={handleInputChange}
-          />
-          <Input
-            type="number"
-            name="mealTimes"
-            title="1日の食事回数(3-6回)"
-            minValue={3}
-            maxValue={6}
-            required={true}
-            onChange={handleInputChange}
-          />
-        </Form>
+        <Form />
       </div>
+      {/*
       <div className="container">
         <Input
           type="number"
@@ -146,7 +74,7 @@ function App() {
           title="1回に摂取するサツマイモの量(g)"
           value={112}
         />
-      </div>
+      </div> */}
     </>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,122 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Form(props) {
-  return <form>{props.children}</form>;
+  const [basalMetabolicRate, setBasalMetabolicRate] = useState(1500);
+  const [intakeCoefficient, setIntakeCoefficient] = useState(1.3);
+  const [bodyWeight, setbodyWeight] = useState(65);
+  const [bodyFat, setBodyFat] = useState(16);
+  const [proteinCoefficient, setProteinCoefficient] = useState(2.5);
+  const [mealTimes, setMealTimes] = useState(3);
+  const submitHandler = e => {
+    e.preventDefault();
+    alert(
+      `入力は、${basalMetabolicRate} ${intakeCoefficient} ${bodyWeight} ${basalMetabolicRate} ${bodyFat} ${proteinCoefficient} ${mealTimes}`
+    );
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <div className="form-input">
+        <div className="form-input">
+          <label htmlFor="basalMetabolicRate" className="required">
+            基礎代謝(1000-4000kcal)
+          </label>
+          <input
+            value={basalMetabolicRate}
+            type="number"
+            name="basalMetabolicRate"
+            id="basalMetabolicRate"
+            min={1000}
+            max={4000}
+            onChange={e => setBasalMetabolicRate(e.target.value)}
+          />
+        </div>
+      </div>
+      <div className="form-input">
+        <div className="form-input">
+          <label htmlFor="intakeCoefficient" className="required">
+            摂取係数(1.3-1.7)
+          </label>
+          <input
+            value={intakeCoefficient}
+            type="number"
+            name="intakeCoefficient"
+            id="intakeCoefficient"
+            min={1.3}
+            max={1.7}
+            step="0.1"
+            onChange={e => setIntakeCoefficient(e.target.value)}
+          />
+        </div>
+      </div>
+      <div className="form-input">
+        <div className="form-input">
+          <label htmlFor="bodyWeight" className="required">
+            体重(kg)
+          </label>
+          <input
+            value={bodyWeight}
+            type="number"
+            name="bodyWeight"
+            id="bodyWeight"
+            min={40}
+            max={90}
+            onChange={e => setbodyWeight(e.target.value)}
+          />
+        </div>
+      </div>
+      <div className="form-input">
+        <div className="form-input">
+          <label htmlFor="bodyFat" className="required">
+            体脂肪(%)
+          </label>
+          <input
+            value={bodyFat}
+            type="number"
+            name="bodyFat"
+            id="bodyFat"
+            min={3}
+            max={25}
+            onChange={e => setBodyFat(e.target.value)}
+          />
+        </div>
+      </div>
+      <div className="form-input">
+        <div className="form-input">
+          <label htmlFor="proteinCoefficient" className="required">
+            タンパク質摂取係数(2-2.5)
+          </label>
+          <input
+            value={proteinCoefficient}
+            type="number"
+            name="proteinCoefficient"
+            id="proteinCoefficient"
+            min={2}
+            max={2.5}
+            step="0.1"
+            onChange={e => setProteinCoefficient(e.target.value)}
+          />
+        </div>
+      </div>
+      <div className="form-input">
+        <div className="form-input">
+          <label htmlFor="mealTimes" className="required">
+            1日の食事回数(3-6回)
+          </label>
+          <input
+            value={mealTimes}
+            type="number"
+            name="mealTimes"
+            id="mealTimes"
+            min={3}
+            max={6}
+            onChange={e => setMealTimes(e.target.value)}
+          />
+        </div>
+      </div>
+      <button>入力テスト</button>
+    </form>
+  );
 }
 
 export default Form;
