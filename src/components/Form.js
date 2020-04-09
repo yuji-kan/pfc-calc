75d770748279579d3b8ee981e5af90bed17a4a47
Yuji Kan <gtenio@gmail.com>
2020-04-10 01:31:34 +0900

    背景画像を入れて試してみる

--- src/components/Form.js
+++ src/components/Form.js
@@ -37,17 +37,17 @@ function Form() {
     setwChickenBreastCalorieIntake(
       bodyWeight !== 0
         ? parseInt(bodyWeight - bodyWeight * (bodyFat / 100)) *
-            proteinCoefficient
+        proteinCoefficient
         : 0
     )
     setwChickenBreast(
       bodyWeight !== 0
         ? parseInt(
-            ((parseInt(bodyWeight - bodyWeight * (bodyFat / 100)) *
-              proteinCoefficient) /
-              food.chickenBreast.protein) *
-              100
-          )
+          ((parseInt(bodyWeight - bodyWeight * (bodyFat / 100)) *
+            proteinCoefficient) /
+            food.chickenBreast.protein) *
+          100
+        )
         : ""
     )
     setwChickenBreastCalorie(
@@ -56,177 +56,145 @@ function Form() {
           ((parseInt(bodyWeight - bodyWeight * (bodyFat / 100)) *
             proteinCoefficient) /
             food.chickenBreast.protein) *
-            100
+          100
         ) /
           100) *
-          food.chickenBreast.calorie
+        food.chickenBreast.calorie
       )
     )
     setwOtherProtainCalorie(
       parseInt(
         basalMetabolicRate * intakeCoefficient -
-          parseInt(
-            (parseInt(
-              ((parseInt(bodyWeight - bodyWeight * (bodyFat / 100)) *
-                proteinCoefficient) /
-                food.chickenBreast.protein) *
-                100
-            ) /
-              100) *
-              food.chickenBreast.calorie
-          )
+        parseInt(
+          (parseInt(
+            ((parseInt(bodyWeight - bodyWeight * (bodyFat / 100)) *
+              proteinCoefficient) /
+              food.chickenBreast.protein) *
+            100
+          ) /
+            100) *
+          food.chickenBreast.calorie
+        )
       )
     )
     setwSweetPotato(
       parseInt(
         ((basalMetabolicRate * intakeCoefficient) / food.sweetPotato.calorie) *
-          100
+        100
       )
     )
     setwChickenBreastOnce(
       mealTimes !== 0
         ? parseInt(
-            parseInt(
-              ((parseInt(bodyWeight - bodyWeight * (bodyFat / 100)) *
-                proteinCoefficient) /
-                food.chickenBreast.protein) *
-                100
-            ) / mealTimes
-          )
+          parseInt(
+            ((parseInt(bodyWeight - bodyWeight * (bodyFat / 100)) *
+              proteinCoefficient) /
+              food.chickenBreast.protein) *
+            100
+          ) / mealTimes
+        )
         : 0
     )
     setwSweetPotatoOnce(
       mealTimes !== 0
         ? parseInt(
-            parseInt(
-              ((basalMetabolicRate * intakeCoefficient) /
-                food.sweetPotato.calorie) *
-                100
-            ) / mealTimes
-          )
+          parseInt(
+            ((basalMetabolicRate * intakeCoefficient) /
+              food.sweetPotato.calorie) *
+            100
+          ) / mealTimes
+        )
         : 0
     )
   }
 
   return (
     <>
-      <form onSubmit={submitHandler}>
-        <div className='container'>
-          <div className='form-input'>
-            <div className='form-input'>
-              <label htmlFor='basalMetabolicRate' className='required'>
-                基礎代謝(1000-2500kcal)
-              </label>
-              <input
-                type='number'
-                name='basalMetabolicRate'
-                id='basalMetabolicRate'
-                min={1000}
-                max={2500}
-                onChange={e => setBasalMetabolicRate(e.target.value)}
-              />
-            </div>
-          </div>
-          <div className='form-input'>
-            <div className='form-input'>
-              <label htmlFor='intakeCoefficient' className='required'>
-                摂取係数(1.3-1.7)ダイエット目的なら低く
-              </label>
-              <input
-                type='number'
-                name='intakeCoefficient'
-                id='intakeCoefficient'
-                min={1.3}
-                max={1.7}
-                step='0.1'
-                onChange={e => setIntakeCoefficient(e.target.value)}
-              />
-            </div>
-          </div>
-          <div className='form-input'>
-            <div className='form-input'>
-              <label htmlFor='bodyWeight' className='required'>
-                体重(40-140kg)
-              </label>
-              <input
-                type='number'
-                name='bodyWeight'
-                id='bodyWeight'
-                min={40}
-                max={140}
-                onChange={e => setbodyWeight(e.target.value)}
-              />
-            </div>
-          </div>
-          <div className='form-input'>
-            <div className='form-input'>
-              <label htmlFor='bodyFat' className='required'>
-                体脂肪(3-25%)
-              </label>
-              <input
-                type='number'
-                name='bodyFat'
-                id='bodyFat'
-                min={3}
-                max={40}
-                onChange={e => setBodyFat(e.target.value)}
-              />
-            </div>
-          </div>
-          <div className='form-input'>
-            <div className='form-input'>
-              <label htmlFor='proteinCoefficient' className='required'>
-                タンパク質摂取係数(2-2.5)多め推奨
-              </label>
-              <input
-                type='number'
-                name='proteinCoefficient'
-                id='proteinCoefficient'
-                min={2}
-                max={2.5}
-                step='0.1'
-                onChange={e => setProteinCoefficient(e.target.value)}
-              />
-            </div>
-          </div>
-          <div className='form-input'>
-            <div className='form-input'>
-              <label htmlFor='mealTimes' className='required'>
-                1日の食事回数(3-6回)食事回数は多いのが◎
-              </label>
-              <input
-                type='number'
-                name='mealTimes'
-                id='mealTimes'
-                min={3}
-                max={6}
-                onChange={e => setMealTimes(e.target.value)}
-              />
-            </div>
-          </div>
-          <button>計算</button>
-        </div>
+      <div className='wrapper'>
+        <form className="contact-form" onSubmit={submitHandler}>
+          <div className="input-fields">
 
-        <div className='container'>
-          <h4>除脂肪体重 : {wLeanBodyMass}kg</h4>
-          <h4>1日のカロリー摂取量 : {wCalorieIntakeDay}kcal</h4>
-          <h4>
-            おすすめの1日に摂取するタンパク質量 : {wChickenBreastCalorieIntake}g
+            <input
+              type='number'
+              name='basalMetabolicRate'
+              id='basalMetabolicRate'
+              min={1000}
+              max={2500}
+              onChange={e => setBasalMetabolicRate(e.target.value)}
+              placeholder="摂取係数(1.3-1.7)ダイエット目的なら低く"
+            />
+            <input
+              type='number'
+              name='intakeCoefficient'
+              id='intakeCoefficient'
+              min={1.3}
+              max={1.7}
+              step='0.1'
+              onChange={e => setIntakeCoefficient(e.target.value)}
+              placeholder="摂取係数(1.3-1.7)ダイエット目的なら低く"
+            />
+            <input
+              type='number'
+              name='bodyWeight'
+              id='bodyWeight'
+              min={40}
+              max={140}
+              onChange={e => setbodyWeight(e.target.value)}
+              placeholder="体重(40-140kg)"
+            />
+            <input
+              type='number'
+              name='bodyFat'
+              id='bodyFat'
+              min={3}
+              max={40}
+              onChange={e => setBodyFat(e.target.value)}
+              placeholder="体脂肪(3-25%)"
+            />
+            <input
+              type='number'
+              name='proteinCoefficient'
+              id='proteinCoefficient'
+              min={2}
+              max={2.5}
+              step='0.1'
+              onChange={e => setProteinCoefficient(e.target.value)}
+              placeholder="タンパク質摂取係数(2-2.5)多め推奨"
+            />
+            <input
+              type='number'
+              name='mealTimes'
+              id='mealTimes'
+              min={3}
+              max={6}
+              onChange={e => setMealTimes(e.target.value)}
+              placeholder="1日の食事回数(3-6回)食事回数は多いのが◎"
+            />
+          </div>
+          <div className="output">
+
+            <h4>除脂肪体重 : {wLeanBodyMass}kg</h4>
+            <h4>1日のカロリー摂取量 : {wCalorieIntakeDay}kcal</h4>
+            <h4>
+              おすすめの1日に摂取するタンパク質量 : {wChickenBreastCalorieIntake}g
           </h4>
-          <h4>1日のタンパク質を摂取できる鶏むね肉の量 : {wChickenBreast}g</h4>
-          <h4>1日に摂取する鶏むね肉のカロリー : {wChickenBreastCalorie}kcal</h4>
-          <h4>タンパク質以外で摂取するカロリー : {wOtherProtainCalorie}kcal</h4>
-          <h4>1日に摂取するサツマイモの量 : {wSweetPotato}g</h4>
-          <h4>
-            食事が1日{mealTimes}回の場合、1回に摂取する鶏むね肉の量 :{" "}
-            {wChickenBreastOnce}g
+            <h4>1日のタンパク質を摂取できる鶏むね肉の量 : {wChickenBreast}g</h4>
+            <h4>1日に摂取する鶏むね肉のカロリー : {wChickenBreastCalorie}kcal</h4>
+            <h4>タンパク質以外で摂取するカロリー : {wOtherProtainCalorie}kcal</h4>
+            <h4>1日に摂取するサツマイモの量 : {wSweetPotato}g</h4>
+            <h4>
+              食事が1日{mealTimes}回の場合、1回に摂取する鶏むね肉の量 :{" "}
+              {wChickenBreastOnce}g
           </h4>
-          <h4>
-            食事が1日{mealTimes}回の場合、1回に摂取するサツマイモの量 :
+            <h4>
+              食事が1日{mealTimes}回の場合、1回に摂取するサツマイモの量 :
             {wSweetPotatoOnce}g
           </h4>
-        </div>
-      </form>
+          </div>
+          <button className="btn">計算</button>
+
+        </form>
+      </div>
     </>
   )
 }
